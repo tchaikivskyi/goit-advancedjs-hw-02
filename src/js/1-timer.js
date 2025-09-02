603c5b25e3deb5c89860131e33a279c25bff407b
chaikivskti.t <chaikivskyi.t@gmail.com>
2025-09-02 15:06:50 +0200

    prepared project structure

---
+++ src/js/1-timer.js
@@ -0,0 +1,10 @@
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,5 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
