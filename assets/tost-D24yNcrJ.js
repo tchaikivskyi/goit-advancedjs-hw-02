a2a4dcd51e6361f7e0abe81370c30ec5f6e6b478
tchaikivskyi <122614214+tchaikivskyi@users.noreply.github.com>
2025-09-02 22:08:08 +0000

    Deploying to gh-pages from @ tchaikivskyi/goit-advancedjs-hw-02@164ed1ce0b0dcb458798056c321dcde69a2dbb8a 🚀

---
+++ assets/tost-D24yNcrJ.js
@@ -0,0 +1,2 @@
+import{i as t}from"./vendor-BbbuE1sJ.js";const r=s=>t.error({title:"Error",message:s,position:"topRight"}),e=s=>t.success({title:"Success",message:s,position:"topRight"});export{r as e,e as s};
+//# sourceMappingURL=tost-D24yNcrJ.js.map
